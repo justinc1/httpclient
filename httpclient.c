7fc7d3185bb1947cdc2f9c6bc5e1700a91d425c5
Justin Cinkelj <justin.cinkelj@xlab.si>
2017-06-28 14:37:56 +0200

    generate 10 HTTP 1.1 requests

--- httpclient.c
+++ httpclient.c
@@ -12,10 +12,12 @@
 #include <unistd.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <errno.h>
+#include <assert.h>
 
 #define BUF_SIZE 1024
 
-int get_request(char * url, char * port);
+int main_loop(char * url, char * port);
 int isValidIP(char * ip);
 
 char path[1000];
@@ -34,7 +36,7 @@ pid_t my_gettid() {
 
 int main(int argc, char**argv) {
     struct sockaddr_in addr, cl_addr;
-    int sockfd, ret;
+    int ret;
     struct hostent * server;
     char * url, * temp;
     int portNumber;
@@ -60,7 +62,7 @@ int main(int argc, char**argv) {
         exit(1);
     }
 
-    sockfd = get_request(url, argv[2]);
+    ret = main_loop(url, argv[2]);
 
     return 0;
 }
@@ -74,16 +76,16 @@ int clnt_connect(char * url, char * port) {
     int ret;
 
     if (isValidIP(url)) { //when an IP address is given
-        sprintf(getrequest, "GET / HTTP/1.0\nHOST: %s\n\n", url);
+        sprintf(getrequest, "GET / HTTP/1.1\nHOST: %s\n\n", url);
     } else { //when a host name is given
         if ((ptr = strstr(url, "/")) == NULL) {
             //when hostname does not contain a slash
-            sprintf(getrequest, "GET / HTTP/1.0\nHOST: %s\n\n", url);
+            sprintf(getrequest, "GET / HTTP/1.1\nHOST: %s\n\n", url);
         } else {
             //when hostname contains a slash, it is a path to file
             strcpy(path, ptr);
             host = strtok(url, "/");
-            sprintf(getrequest, "GET %s HTTP/1.0\nHOST: %s\n\n", path, url);
+            sprintf(getrequest, "GET %s HTTP/1.1\nHOST: %s\n\n", path, url);
         }
     }
 
@@ -114,7 +116,7 @@ int clnt_connect(char * url, char * port) {
 int clnt_send_request() {
     int ret;
     ret = write(sockfd, getrequest, strlen(getrequest));
-    debug(" sent %d of %d bytes\n", ret, strlen(getrequest));
+    //debug(" INFO sent %d of %d bytes\n", ret, strlen(getrequest));
     if (ret < strlen(getrequest)) {
         printf("ERR clnt_send_request %d\n", ret);
         return -1;
@@ -127,42 +129,73 @@ int clnt_recv_respose() {
     char * url, * temp;
     char buffer[BUF_SIZE];
     char http_ok[] = "HTTP/1.1 200 OK\r\n";
+    char http_header_term[] = "\r\n\r\n";
+    char http_content_length[] = "\r\nContent-Length: ";
     int buf_pos = 0;
+    bool response_done = false;
+    int header_length = 0;
+    int content_length = 0;
 
     memset(&buffer, 0, sizeof(buffer));
-    ret = recv(sockfd, buffer, BUF_SIZE, 0);
-    if (ret < 0) {
-        printf("Error receiving HTTP status!\n");
-    } else {
-        buf_pos += ret;
-        printf("%s\n", buffer);
-        if ((temp = strstr(buffer, http_ok)) == NULL) {
-            debug(" recv-1\n");
-            close(sockfd);
-            sockfd = -1;
-            return 1;
+    while(response_done == false) {
+        ret = recv(sockfd, buffer, BUF_SIZE, 0);
+        //debug(" INFO recv ret=%d, errno=%d\n", ret, errno); sleep(1);
+        if (ret < 0) {
+            printf("Error receiving HTTP status!\n");
+        }
+        else if (ret == 0) {
+            if (errno == 0) {
+                debug(" INFO recv ret=%d, errno=%d, server closed connection?\n", ret, errno);
+                break;
+            }
+        }
+        else {
+            //printf("%s\n", buffer);
+            // reply should start with "HTTP OK".
+            if (buf_pos == 0 &&
+                (temp = strstr(buffer, http_ok)) == NULL) {
+                debug(" ERROR reply is not HTTP 200 OK\n");
+                close(sockfd);
+                sockfd = -1;
+                return 1;
+            }
+            buf_pos += ret;
+            if (header_length == 0) {
+                if ((temp = strstr(buffer, http_header_term)) == NULL) {
+                    debug(" ERROR reply is missing http_header_term\n");
+                    close(sockfd);
+                    sockfd = -1;
+                    return 1;
+                }
+                header_length += temp + strlen(http_header_term) - buffer;
+                //debug(" INFO header_length=%d\n", header_length);
+            }
+            if (content_length == 0) {
+                if ((temp = strstr(buffer, http_content_length)) == NULL) {
+                    debug(" ERROR reply is missing Content-Length\n");
+                    close(sockfd);
+                    sockfd = -1;
+                    return 1;
+                }
+                temp += strlen(http_content_length);
+                content_length = atoi(temp);
+                //debug(" INFO content_length=%d\n", content_length);
+            }
+            if (content_length > 0) {
+                assert(header_length > 0);
+                //debug(" INFO  header_length=%d content_length=%d, buf_pos=%d\n", header_length, content_length, buf_pos);
+                if(buf_pos == header_length+content_length) {
+                    response_done = true;
+                }
+            }
         }
     }
 
-    memset(&buffer, 0, sizeof(buffer));
-    ret = recv(sockfd, buffer, BUF_SIZE, 0);
-    if (ret < 0) {
-        printf("Error receiving HTTP header!\n");
-    } else {
-        buf_pos += ret;
-        printf("%s\n", buffer);
-        if (0 /*parseHeader(buffer) != 0*/) {
-            debug(" recv-2\n");
-            close(sockfd);
-            sockfd = -1;
-            return 1;
-        }
-    }
     return 0;
 }
 
 
-int get_request(char * url, char * port) {
+int main_loop(char * url, char * port) {
     int ret;
     ret = clnt_connect(url, port);
     if (ret) {
@@ -171,19 +204,24 @@ int get_request(char * url, char * port) {
     }
 
 // writes the HTTP GET Request to the sockfd
-    ret = clnt_send_request();
-    if (ret) {
-        printf("ERR clnt_send_request %d\n", ret);
-        return ret;
+    int nreq, ii;
+    nreq = 10;
+    for (ii=0; ii<nreq; ii++) {
+        ret = clnt_send_request();
+        if (ret) {
+            printf("ERR clnt_send_request %d\n", ret);
+            return ret;
+        }
+
+        ret = clnt_recv_respose();
+        if (ret) {
+            printf("ERR clnt_recv_respose %d\n", ret);
+            return ret;
+        }
     }
 
-    ret = clnt_recv_respose();
-    if (ret) {
-        printf("ERR clnt_recv_respose %d\n", ret);
-        return ret;
-    }
-
-    return sockfd;
+    close(sockfd);
+    sockfd = -1;
 }
 
 
