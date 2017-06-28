bd0bca919b74eb800e1f41a2947bb2630481d62e
Justin Cinkelj <justin.cinkelj@xlab.si>
2017-06-28 13:12:00 +0200

    refactor code
    create functions clnt_connect, clnt_send_request and clnt_recv_respose

--- httpclient.c
+++ httpclient.c
@@ -1,12 +1,17 @@
 // http://www.theinsanetechie.in/2014/02/a-simple-http-client-and-server-in-c.html
 /**httpclient.c**/
-#include"stdio.h"
-#include"stdlib.h"
-#include"sys/types.h"
-#include"sys/socket.h"
-#include"string.h"
-#include"netinet/in.h"
-#include"netdb.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "sys/types.h"
+#include "sys/socket.h"
+#include "string.h"
+#include "netinet/in.h"
+#include "netdb.h"
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #define BUF_SIZE 1024
 
@@ -22,20 +27,24 @@ char status[4] = {0, 0, 0, 0};
 char contentFileType[100];
 char path[1000];
 
+
+/*extern "C"*/ pid_t gettid() __attribute((weak));
+pid_t my_gettid() {
+    if (gettid)
+        return gettid();
+    else
+        return getpid();
+}
+
+#define debug(fmt, ...) if(1) { fprintf(stderr, "%s:%d tid=% 5d %s: " fmt, __FILE__, __LINE__, my_gettid(), __FUNCTION__, ##__VA_ARGS__); }
+
+
 int main(int argc, char**argv) {
     struct sockaddr_in addr, cl_addr;
     int sockfd, ret;
     struct hostent * server;
     char * url, * temp;
     int portNumber;
-    char * fileName;
-    char status_ok[] = "OK";
-    char buffer[BUF_SIZE];
-    char http_not_found[] = "HTTP/1.0 404 Not Found";
-    char http_ok[] = "HTTP/1.0 200 OK";
-    char location[] = "Location: ";
-    char contentType[] = "Content-Type: ";
-    int sPos, ePos;
 
     if (argc < 3) {
         printf("usage: [URL] [port number]\n");
@@ -60,68 +69,16 @@ int main(int argc, char**argv) {
 
     sockfd = get_request(url, argv[2]);
 
-    memset(&buffer, 0, sizeof(buffer));
-    ret = recv(sockfd, buffer, BUF_SIZE, 0);
-    if (ret < 0) {
-        printf("Error receiving HTTP status!\n");
-    } else {
-        printf("%s\n", buffer);
-        if ((temp = strstr(buffer, http_ok)) != NULL) {
-            send(sockfd, status_ok, strlen(status_ok), 0);
-        } else {
-            close(sockfd);
-            return 0;
-        }
-    }
-
-    memset(&buffer, 0, sizeof(buffer));
-    ret = recv(sockfd, buffer, BUF_SIZE, 0);
-    if (ret < 0) {
-        printf("Error receiving HTTP header!\n");
-    } else {
-        printf("%s\n", buffer);
-        if (parseHeader(buffer) == 0) {
-            send(sockfd, status_ok, strlen(status_ok), 0);
-        } else {
-            printf("Error in HTTP header!\n");
-            close(sockfd);
-            return 0;
-        }
-    }
-
-//printf("file: [%s]\n", fileName);
-    fileptr = fopen(path, "w");
-    if (fileptr == NULL) {
-        printf("Error opening file!\n");
-        close(sockfd);
-        return 0;
-    }
-
-    memset(&buffer, 0, sizeof(buffer));
-    while (recv(sockfd, buffer, BUF_SIZE, 0) > 0) { //receives the file
-        if ((strstr(contentFileType, "text/html")) != NULL) {
-            fprintf(fileptr, "%s", buffer);
-        } else {
-            fwrite(&buffer, sizeof(buffer), 1, fileptr);
-        }
-        memset(&buffer, 0, sizeof(buffer));
-    }
-
-    fclose(fileptr);
-    close(sockfd);
-
-    openFile();
-
     return 0;
 }
 
+int sockfd = -1;
+char getrequest[1024];
 
-int get_request(char * url, char * port) {
-
-    int sockfd, bindfd;
+int clnt_connect(char * url, char * port) {
     char * ptr, * host;
-    char getrequest[1024];
     struct sockaddr_in addr;
+    int ret;
 
     if (isValidIP(url)) { //when an IP address is given
         sprintf(getrequest, "GET / HTTP/1.0\nHOST: %s\n\n", url);
@@ -157,6 +114,69 @@ int get_request(char * url, char * port) {
     printf("Connection successful...\n\n\n");
     ptr = strtok(path, "/");
     strcpy(path, ptr);
+
+    return 0;
+}
+
+int clnt_send_request() {
+    int ret;
+    ret = write(sockfd, getrequest, strlen(getrequest));
+    if (ret < strlen(getrequest)) {
+        printf("ERR clnt_send_request %d\n", ret);
+        return -1;
+    }
+    return 0;
+}
+
+int clnt_recv_respose() {
+    int ret;
+    char * url, * temp;
+    char status_ok[] = "OK";
+    char buffer[BUF_SIZE];
+    char http_not_found[] = "HTTP/1.0 404 Not Found";
+    char http_ok[] = "HTTP/1.0 200 OK";
+    char location[] = "Location: ";
+    char contentType[] = "Content-Type: ";
+    int sPos, ePos;
+
+    memset(&buffer, 0, sizeof(buffer));
+    ret = recv(sockfd, buffer, BUF_SIZE, 0);
+    if (ret < 0) {
+        printf("Error receiving HTTP status!\n");
+    } else {
+        printf("%s\n", buffer);
+        if ((temp = strstr(buffer, http_ok)) != NULL) {
+            send(sockfd, status_ok, strlen(status_ok), 0);
+        } else {
+            close(sockfd);
+            return 0;
+        }
+    }
+
+    memset(&buffer, 0, sizeof(buffer));
+    ret = recv(sockfd, buffer, BUF_SIZE, 0);
+    if (ret < 0) {
+        printf("Error receiving HTTP header!\n");
+    } else {
+        printf("%s\n", buffer);
+        if (parseHeader(buffer) == 0) {
+            send(sockfd, status_ok, strlen(status_ok), 0);
+        } else {
+            printf("Error in HTTP header!\n");
+            close(sockfd);
+            return 0;
+        }
+    }
+}
+
+
+int get_request(char * url, char * port) {
+    int ret;
+    ret = clnt_connect(url, port);
+    if (ret) {
+        printf("ERR clnt_connect %d\n", ret);
+        return ret;
+    }
 //printf("path=%s\n", path);
 //fileptr = fopen(path, "w");
 //strcpy(fileName, path);
@@ -166,7 +186,17 @@ int get_request(char * url, char * port) {
     //setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof optval);
 
 // writes the HTTP GET Request to the sockfd
-    write(sockfd, getrequest, strlen(getrequest));
+    ret = clnt_send_request();
+    if (ret) {
+        printf("ERR clnt_send_request %d\n", ret);
+        return ret;
+    }
+
+    ret = clnt_recv_respose();
+    if (ret) {
+        printf("ERR clnt_send_request %d\n", ret);
+        return ret;
+    }
 
     return sockfd;
 }
